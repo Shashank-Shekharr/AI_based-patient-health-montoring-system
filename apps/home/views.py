3568bc883588a0f7905b88f0f7defe389b9b8342
Shashank-Shekharr <61085117+Shashank-Shekharr@users.noreply.github.com>
2023-04-15 14:48:17 +0000

    ok

--- apps/home/views.py
+++ apps/home/views.py
@@ -33,7 +33,9 @@ def pages(request):
     #context = super().get_context_data(**kwargs)
     #context["qs"] = Editors.objects.all()
     patients =Patient.objects.all()
-    context={"pulse":l2,"oxi":l,"tempa":l3,"patient":patients}
+    mydata = Patient.objects.all().values()
+
+    context={"pulse":l2,"oxi":l,"tempa":l3,"patient":mydata}
     # All resource paths end in .html.
     # Pick out the html file name from the url. And load that template.
     try:
